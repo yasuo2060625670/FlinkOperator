aab2013ce96769b625a5ad3c4d16d45105a299d7
trx_gk <zhuwei@topsec.com.cn>
2020-08-04 14:57:44 +0800

    0804

--- Operators/src/main/java/AccOperator/AccOperator.java
+++ Operators/src/main/java/AccOperator/AccOperator.java
@@ -2,9 +2,14 @@ package AccOperator; /**
  * @author ：zz
  * @date ：Created in 2020/6/22 10:44
  */
+
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.table.api.Types;
 import AccOperator.functions.AccFunction;
 import AccOperator.functions.AccProcessWindowFunction;
 import AccOperator.functions.TimeIntervalTrigger;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.runtime.state.memory.MemoryStateBackend;
 import org.apache.flink.runtime.state.CheckpointListener;
@@ -35,32 +40,30 @@ public class AccOperator {
             @Override
             public void run(SourceContext<Row> ctx) throws Exception {
                 while (true) {
-                    count++;
+//                    count++;
                     Date date = new Date(System.currentTimeMillis());
-                    String str = "中国银行";
-                    if (count >=10)
-                    {
-                        str = "";
-                    }
-                    ctx.collect(Row.of("192.168.1.1",count, str, date.toString()));
-                    Thread.sleep(1000);
+//                    String str = "中国银行";
+//                    if (count >=10)
+//                    {
+//                        str = "";
+//                    }
+//                    ctx.collect(Row.of("192.168.1.1",count, str, date.toString()));
+//                    Thread.sleep(1000);
                     count++;
                     Date date2 = new Date(System.currentTimeMillis());
-                    ctx.collect(Row.of("192.168.1.2",count, "建设银行", date2.toString()));
+                    ctx.collect(Row.of("192.168.1.2",count, "192.168.1.3", date2.toString()));
                     Thread.sleep(1000);
                     count++;
                     Date date3 = new Date(System.currentTimeMillis());
-                    ctx.collect(Row.of("192.168.1.3",count, "工商银行", date3.toString()));
+                    ctx.collect(Row.of("192.168.1.3",count, "192.168.1.2", date3.toString()));
                     Thread.sleep(1000);
                     Date date4 = new Date(System.currentTimeMillis());
-                    ctx.collect(Row.of("192.168.1.2",count, "建设银行", date4.toString()));
+                    ctx.collect(Row.of("192.168.1.9",count, "192.168.1.3", date4.toString()));
                     Thread.sleep(1000);
                     Date date5 = new Date(System.currentTimeMillis());
-                    ctx.collect(Row.of("192.168.1.3",count, "工商银行", date5.toString()));
+                    ctx.collect(Row.of("192.168.1.3",count, "192.168.1.9", date5.toString()));
                     Thread.sleep(1000);
-                    Date date6 = new Date(System.currentTimeMillis());
-                    ctx.collect(Row.of("192.168.1.1",count, "广发银行", date6.toString()));
-                    Thread.sleep(1000);
+
                 }
             }
             @Override
@@ -68,7 +71,21 @@ public class AccOperator {
 
             }
         });
-        int[] arr = {2};
+        DataStream<Row> map = rowDataStreamSource.map(new MapFunction<Row, Row>() {
+            @Override
+            public Row map(Row row) throws Exception {
+                Row row1 = new Row(row.getArity() + 1);
+                for (int i = 0; i < row.getArity() + 1; i++) {
+                    if (i != row.getArity()) {
+                        row1.setField(i, row.getField(i));
+                    } else {
+                        row1.setField(i , String.valueOf(row.getField(0).hashCode() + row.getField(2).hashCode()));
+                    }
+                }
+                return row1;
+            }
+        });
+        int[] arr = {4};
 //        env.enableCheckpointing(1000);
 //        env.setStateBackend(new MemoryStateBackend());
 //        env.enableCheckpointing(1000);
@@ -84,19 +101,22 @@ public class AccOperator {
 //        env.getCheckpointConfig().setMaxConcurrentCheckpoints(1);
 //        env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
-        SingleOutputStreamOperator<Row> ds2 = rowDataStreamSource.assignTimestampsAndWatermarks(new AscendingTimestampExtractor<Row>() {
+        SingleOutputStreamOperator<Row> ds2 = map.assignTimestampsAndWatermarks(new AscendingTimestampExtractor<Row>() {
             @Override
             public long extractAscendingTimestamp(Row row) {
                 return System.currentTimeMillis();
             }
         });
+
+
         SingleOutputStreamOperator<Row> returns = ds2.keyBy(row -> Row.project(row, arr))
                 .window(TumblingEventTimeWindows.of(Time.days(1)))
                 .trigger(new TimeIntervalTrigger(5000))
-                .aggregate(new AccFunction(2, 1),
+                .aggregate(new AccFunction(4, 1),
                         new AccProcessWindowFunction()
                 ).returns(Types.ROW(new String[]{"institution_short", "inst_count", "inst_sum"}, new TypeInformation[]{Types.STRING(), Types.LONG(), Types.LONG()}));
         returns.print();
+//        map.print();
         env.execute();
 
 
